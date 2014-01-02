1e1d3d16130fe9ae5178cd136882dd47bb9493b7
Julian C. Dunn <jdunn@getchef.com>
2014-01-02 16:15:20 -0500

    Fix deps and description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'xcode'
 maintainer       'Urbandecoder Labs'
 maintainer_email 'jdunn@aquezada.com'
 license          'Apache 2.0'
-description      'Installs/Configures Apple XCode'
+description      'Installs Apple XCode and command-line tools'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 supports         'mac_os_x'
